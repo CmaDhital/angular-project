8104480ec6647c6261b5d509908061cb9920858b
Seema Dhital <cmadhital@outlook.com>
2016-08-05 10:27:49 -0500

    added more in index

---
+++ js/directives/carousel.js
@@ -0,0 +1,17 @@
+angular.module('app').directive('carouselDir', [function($scope){
+
+    return {
+        restrict:'EA',
+        templateUrl: "/angular-project/views/carousel.html",
+        link:function($scope)
+        {
+
+
+
+
+        }
+
+    };
+
+
+}]);
---
+++ js/directives/formDir.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Seema on 8/4/2016.
+ */
+
+angular.module('app').directive('formDir', [function($scope){
+
+    return {
+        restrict:'EA',
+        templateUrl: "/angular-project/views/form.html",
+        link:function($scope)
+        {
+
+
+
+
+        }
+
+    };
+
+
+}]);
